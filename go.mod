7244b563ef0f07dbb1a3738ee18b76d985692ef4
Ryuto Shimoji <e205755@ie.u-ryukyu.ac.jp>
2022-10-03 18:25:10 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.ie.u-ryukyu.ac.jp/os/2022/e205755-filewrite
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.ie.u-ryukyu.ac.jp/os/2022/e205755-filewrite/fileWrite"
+)
+func main() {
+		fmt.Println("Hello, World!")
+		fmt.Println( fileWrite.Hello("e205755"))
+}
